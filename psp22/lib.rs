8b0c0919b5d17dbe0b01673bde03eda7e97ef1de
rottenkiwi <yonatanhornstein@gmail.com>
2022-06-22 20:34:20 +0300

    Refactored SC

---
+++ psp22/lib.rs
@@ -0,0 +1,51 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![feature(min_specialization)]
+
+#[openbrush::contract]
+pub mod my_psp22 {
+    
+    use ink_prelude::string::String;
+    use ink_storage::traits::SpreadAllocate;
+    use openbrush::contracts::psp22::*;
+    use openbrush::contracts::psp22::extensions::metadata::*;
+
+    #[ink(storage)]
+    #[derive(Default, SpreadAllocate, PSP22Storage,PSP22MetadataStorage)]
+    pub struct MyPSP22 {
+        #[PSP22StorageField]
+        psp22: PSP22Data,
+        #[PSP22MetadataStorageField]
+        metadata: PSP22MetadataData
+
+    }
+
+
+    impl PSP22Metadata for MyPSP22 {}
+    impl PSP22 for MyPSP22 {}
+
+    impl MyPSP22 {
+        #[ink(constructor)]
+        pub fn new(total_supply: Balance, name: Option<String>, symbol: Option<String>, decimal: u8) -> Self {
+            ink_lang::codegen::initialize_contract(|instance: &mut MyPSP22| {
+                instance
+                    ._mint(instance.env().caller(), total_supply)
+                    .expect("Should mint");
+
+                instance.metadata.name = name;
+                instance.metadata.symbol = symbol;
+                instance.metadata.decimals = decimal;
+            })
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ vesting_contract/lib.rs
@@ -0,0 +1,350 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+extern crate chrono;
+#[cfg(not(feature = "ink-as-dependency"))]
+
+
+
+#[ink::contract]
+pub mod vesting_contract {
+
+
+    use chrono::prelude::*;
+    use ink_storage::traits::SpreadAllocate;
+    
+    #[cfg(not(feature = "ink-as-dependency"))]
+    use openbrush::{
+        contracts::{
+
+            traits::psp22::PSP22Ref,
+        },
+    };
+    use ink_env::call::FromAccountId;
+    
+    use ink_env::CallFlags;
+    use ink_prelude::vec::Vec;
+    use ink_prelude::string::ToString;
+    use ink_prelude::string::String;
+    use ink_prelude::borrow::ToOwned;
+    
+    
+    #[ink(storage)]
+    #[derive(SpreadAllocate)]
+    pub struct VestingContract {
+        
+        //Deployer address 
+        manager: AccountId,
+        //PANX psp22 contract address
+        panx_psp22: AccountId,
+        //Vesting contract deploy date in tsp 
+        started_date_in_timestamp:u64,
+        //Vesting contract deploy date in UTC
+        started_date_in_string: String,
+        //Vesting contract PANX reserve
+        panx_reserve: Balance,
+        //Locked PANX amount for users
+        balances: ink_storage::Mapping<AccountId, Balance>,
+        // 0 didnt collect, 1 did collect.
+        collected_tge: ink_storage::Mapping<AccountId, i64>,
+        //panx reward for a day, for each account
+        panx_to_give_in_a_day: ink_storage::Mapping<AccountId, Balance>,
+        //last time account redeemed
+        last_redeemed:ink_storage::Mapping<AccountId, u64>,
+        //Logging claiming  into vec
+        claiming_log:  ink_storage::Mapping<AccountId, String>,
+        
+
+
+    }
+
+    impl VestingContract {
+        /// Creates a new instance of this contract.
+        #[ink(constructor)]
+        pub fn new(panx_contract:AccountId) -> Self {
+            
+            let me = ink_lang::utils::initialize_contract(|contract: &mut Self| {
+                contract.panx_psp22 = panx_contract;  
+                contract.started_date_in_string = contract.get_current_timestamp_as_utc_string();
+                contract.started_date_in_timestamp = contract.get_current_timestamp();
+                contract.manager = Self::env().caller();
+               
+            });
+            
+            me
+            
+        }
+
+
+        //adding seed events participants to vesting contract and their PANX vesting allocation
+        //Only manager can use this function
+        #[ink(message)]
+        pub fn add_to_vesting_map(&mut self,account:AccountId,panx_to_give_overall:Balance)  {
+
+           //Making sure caller is the manager (Only manager get add)
+           assert!(self.env().caller() == self.manager);
+           //get current account balance (If any)
+           let account_balance = self.balances.get(&account).unwrap_or(0);
+           //add PANX allocation to account
+           self.balances.insert(account, &(account_balance + panx_to_give_overall));
+           //calc how many tokens to give in a day
+           let amount_to_give_each_day = panx_to_give_overall / 365  ;
+           //insert the daily amount to account
+           self.panx_to_give_in_a_day.insert(account,&amount_to_give_each_day);
+           //Allow account to collect TGE
+           self.collected_tge.insert(account,&0);
+           //Insert the current date as last redeem date for account.
+           self.last_redeemed.insert(account, &self.get_current_timestamp());
+           
+           
+           
+        }
+        //fun to collect TGE (10%) for account
+        #[ink(message)]
+        pub fn collect_tge_tokens(&mut self)  {
+
+           //caller address
+           let account = self.env().caller();
+           //current account locked tokens
+           let account_balance = self.balances.get(&account).unwrap_or(0);
+           //making sure account didnt redeem tge yet
+           assert!(self.collected_tge.get(&account).unwrap_or(0) == 0);
+           //making sure account has more then 0 tokens
+           assert!(account_balance > 0);
+
+           //account balance without TGE token amounts
+           let account_balance_with_tge_amount =  (account_balance * 900) / 1000;
+
+           //calc the amount of TGE tokens to give 
+           let amount_to_give = account_balance - account_balance_with_tge_amount;
+
+           //transfers the TGE tokens to caller
+           let response = PSP22Ref::transfer(&self.panx_psp22, self.env().caller(), amount_to_give, ink_prelude::vec![]);
+           
+           //deducts from overall vesting amount to give
+           self.balances.insert(account, &(account_balance - amount_to_give));
+
+           //make sure to change his collected tge status to 1 to prevent the user to call it again
+           self.collected_tge.insert(account,&1);
+
+           let date_as_string = &self.get_current_timestamp_as_utc_string();
+
+           //casting str on the amount given to use in as log
+           let mut amount_given_str = amount_to_give.to_string();
+           //building a whole string of recorded trans
+           let str_to_log = "toknes: ".to_owned()+ &amount_given_str +", date:"+ date_as_string+", type: TGE; ";
+           //pushing log into logs vec
+
+           
+           let current_log = self.claiming_log.get(&self.env().caller()).unwrap_or("".to_string());
+
+           self.claiming_log.insert(self.env().caller(), &(current_log.to_string() + &str_to_log));
+
+        }
+
+        //fun to get account redeemable amount of tokens
+        #[ink(message)]
+        pub fn get_redeemable_amount(&mut self) -> Balance {
+
+            
+            //call address 
+            let account = self.env().caller();
+            //current timestamp
+            let current_tsp = self.get_current_timestamp();
+            //account total locked PANX amount
+            let account_total_vesting_amount = self.get_account_total_vesting_amount(account);
+
+        
+            //last time account (caller) redeemed tokens
+            let last_redeemed = self.last_redeemed.get(account).unwrap_or(0);
+            //How many PANX tokens to give to account each day
+            let panx_to_give_each_day = self.panx_to_give_in_a_day.get(account).unwrap_or(0);
+            //days since last redeem
+            let days_diff = (current_tsp - last_redeemed) / 86400;
+            //making sure that 24 hours has passed since last redeem
+            assert!(days_diff > 0);
+            //making sure that account has more then 0 PANX to redeem
+            assert!(account_total_vesting_amount >= 0);
+            //amount to give to caller
+            let mut amount_redeemable_amount = panx_to_give_each_day * days_diff as u128;
+
+
+            //if account has less tokens from the daily amount, give him the rest of tokens
+            if amount_redeemable_amount > account_total_vesting_amount{
+
+                amount_redeemable_amount = account_total_vesting_amount
+            }
+
+            amount_redeemable_amount
+
+            
+
+        }
+
+        //fun for account to redeem his redeemable tokens.
+        #[ink(message)]
+        pub fn redeem_redeemable_amount(&mut self) {
+
+            
+            //caller address
+            let account = self.env().caller();
+            //current timestamp
+            let current_tsp = self.get_current_timestamp();
+            //caller total locked PANX 
+            let account_total_vesting_amount = self.get_account_total_vesting_amount(account);
+
+        
+            //timestamp for the last redeem data of caller
+            let last_redeemed = self.last_redeemed.get(account).unwrap_or(0);
+            //PANX amount to give caller in each passing day
+            let panx_to_give_each_day = self.panx_to_give_in_a_day.get(account).unwrap_or(0);
+            //difference between last redeem tsp with the current tsp
+            let days_diff = (current_tsp - last_redeemed) / 86400;
+            //making sure more then 24 hours has passed
+            assert!(days_diff > 0);
+            //making sure caller has more then 0 locked PANX
+            assert!(account_total_vesting_amount >= 0);
+            //amount to give to caller 
+            let mut amount_redeemable_amount = panx_to_give_each_day * days_diff as u128;
+
+            //if account has less tokens from the daily amount, give him the rest of tokens
+            if amount_redeemable_amount > account_total_vesting_amount{
+
+                amount_redeemable_amount = account_total_vesting_amount;
+            }
+
+            //Making sure to set his last redeem to current timestamp
+            self.last_redeemed.insert(account,&current_tsp);
+
+            //make sure to deducte from overall amount
+            self.balances.insert(account, &(account_total_vesting_amount -  amount_redeemable_amount));
+
+            //cross contract call to PANX contract to transfer PANX to caller
+            let response = PSP22Ref::transfer(&self.panx_psp22, self.env().caller(), amount_redeemable_amount, ink_prelude::vec![]);
+
+            let date_as_string = &self.get_current_timestamp_as_utc_string();
+
+
+            //casting str on the amount given to use as log
+            let mut amount_given_str = amount_redeemable_amount.to_string();
+            //building a whole string of recorded trans
+            let str_to_log = "toknes: ".to_owned()+ &amount_given_str +", date:"+ date_as_string+", type: CLAIM; ";
+            //pushing log into logs vec
+ 
+            
+            let current_log = self.claiming_log.get(&self.env().caller()).unwrap_or("".to_string());
+ 
+            self.claiming_log.insert(self.env().caller(), &(current_log.to_string() + &str_to_log));
+        
+        }
+
+
+
+
+        //fun to get acount total locked tokens
+        #[ink(message)]
+        pub fn get_account_total_vesting_amount(&mut self,account:AccountId)->Balance  {
+        
+           let account_balance = self.balances.get(&account).unwrap_or(0);
+           account_balance
+
+        }
+        //fun to get account last redeem in timestamp
+        #[ink(message)]
+        pub fn get_account_last_redeem(&mut self,account:AccountId)->u64  {
+        
+           let time_stamp = self.last_redeemed.get(&account).unwrap_or(0);
+           time_stamp
+
+        }
+        //fun to get the amount of tokens to give to account each day.
+        #[ink(message)]
+        pub fn get_amount_to_give_each_day_to_account(&mut self,account:AccountId)->Balance  {
+        
+           let account_balance = self.panx_to_give_in_a_day.get(&account).unwrap_or(0);
+           account_balance
+
+        }
+        //fun to get vesting contract PANX amount
+        #[ink(message)]
+        pub fn get_vesting_contract_panx_reserve(&self)->Balance  {
+        
+            let balance1 = PSP22Ref::balance_of(&self.panx_psp22, Self::env().account_id());
+            balance1
+
+
+        }
+        //fun to get status on TGE collection
+        #[ink(message)]
+        pub fn user_tge_collection_status(&mut self,account:AccountId)->i64  {
+
+            let tge_status = self.collected_tge.get(account).unwrap_or(0);
+            tge_status
+
+
+        }
+        //fun to get the started date in timpstamp and str
+        #[ink(message)]
+        pub fn get_started_date(&self) -> (u64,String) {
+            let timestamp = self.started_date_in_timestamp;
+            let timestamp_str = &self.started_date_in_string;
+            (timestamp,timestamp_str.to_string())
+        }
+
+        
+        //fun to get current timpstamp
+        #[ink(message)]
+        pub fn get_current_timestamp(&self) -> u64 {
+            let bts = self.env().block_timestamp() / 1000;
+            bts
+        }
+
+        
+        //fun to get current timpstamp as str
+        #[ink(message)]
+        pub fn get_current_timestamp_as_utc_string(&self) -> String {
+            let bts = self.env().block_timestamp() / 1000;
+            
+            let naive_datetime = NaiveDateTime::from_timestamp(bts as i64, 0);
+            let datetime_again: DateTime<Utc> = DateTime::from_utc(naive_datetime, Utc);
+            datetime_again.to_string()
+        }
+
+        //fun to get claim log of account
+        #[ink(message)]
+        pub fn get_claim_log(&self,of: AccountId) -> String {
+            let of_log = self.claiming_log.get(&of).unwrap_or("0".to_string());
+
+            of_log
+        }
+
+        //fun to change account tge status
+        #[ink(message)]
+        pub fn change_tge_status_to_0(&mut self,of: AccountId)  {
+
+            self.collected_tge.insert(of, &(0));
+
+            
+        }
+
+        //fun to change balance amount for account
+        #[ink(message)]
+        pub fn change_balance_amount(&mut self,of: AccountId,value:Balance)  {
+
+            self.balances.insert(of, &(value));
+
+            
+        }
+
+        //get the days pass since deployment
+        #[ink(message)]
+        pub fn get_days_passed_since_issue(&self) -> u64 {
+            let current_tsp = self.env().block_timestamp() / 1000;
+
+            let days_diff = (current_tsp - self.started_date_in_timestamp) / 86400;
+
+            days_diff
+        }
+ 
+    }
+}
