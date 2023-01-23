0dcf4eef3b7ee7a3894e47598d226639af084aa2
rottenkiwi <yonatanhornstein@gmail.com>
2023-01-23 11:11:31 +0100

    Updated PSP22 smart contract to INK 4.0 beta

--- psp22/lib.rs
+++ psp22/lib.rs
@@ -1,5 +1,3 @@
-
-
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(min_specialization)]
 
@@ -7,52 +5,40 @@
 pub mod my_psp22 {
 
 
-    use ink_prelude::string::String;
-    //use openbrush::traits::String;
-    use ink_storage::traits::SpreadAllocate;
     use openbrush::{
         contracts::psp22::extensions::metadata::*,
-        traits::Storage,
+        traits::{
+            Storage,
+            String,
+        },
     };
-    use crate::my_psp22::psp22::Internal;
-
 
     #[ink(storage)]
-    #[derive(Default, SpreadAllocate, Storage)]
+    #[derive(Default, Storage)]
     pub struct Contract {
         #[storage_field]
         psp22: psp22::Data,
         #[storage_field]
         metadata: metadata::Data,
-
     }
 
-    impl Internal for Contract {
-        fn _emit_transfer_event(&self, _from: Option<AccountId>, _to: Option<AccountId>, _amount: Balance){}
-        fn _emit_approval_event(&self, _owner: AccountId, _spender: AccountId, _amount: Balance){}
-    }
-
-    impl PSP22 for Contract {
-
-    }
-
-
+    impl PSP22 for Contract {}
 
     impl PSP22Metadata for Contract {}
 
     impl Contract {
         #[ink(constructor)]
         pub fn new(total_supply: Balance, name: Option<String>, symbol: Option<String>, decimal: u8) -> Self {
-            ink_lang::codegen::initialize_contract(|instance: &mut Self| {
-                instance.metadata.name = name;
-                instance.metadata.symbol = symbol;
-                instance.metadata.decimals = decimal;
-                instance
-                    ._mint(instance.env().caller(), total_supply)
-                    .expect("Should mint total_supply");
-            })
+            let mut instance = Self::default();
+
+            instance.metadata.name = name;
+            instance.metadata.symbol = symbol;
+            instance.metadata.decimals = decimal;
+            instance
+                ._mint_to(instance.env().caller(), total_supply)
+                .expect("Should mint total_supply");
+
+            instance
         }
-
-
     }
 }
