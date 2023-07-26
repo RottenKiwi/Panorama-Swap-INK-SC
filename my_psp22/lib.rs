ceed8772edbee1ac23685dd50f45ee5e5dd0bcbf
rottenkiwi <yonatanhornstein@gmail.com>
2023-07-26 12:28:20 +0200

    Upgraded the PSP22 token smart contract

--- my_psp22/lib.rs
+++ my_psp22/lib.rs
@@ -13,8 +13,36 @@ pub mod my_psp22 {
             String,
         },
     };
+    use ink::{
+        codegen::{EmitEvent, Env},
+        reflect::ContractEventBase,
+    };
+    use ink::storage::Mapping;
 
+    
 
+    /// Event emitted when a token transfer occurs.
+    #[ink(event)]
+    #[derive(Debug)]
+    pub struct Transfer {
+        #[ink(topic)]
+        pub from: Option<AccountId>,
+        #[ink(topic)]
+        pub to: Option<AccountId>,
+        pub value: Balance,
+    }
+
+    /// Event emitted when an approval occurs that `spender` is allowed to withdraw
+    /// up to the amount of `value` tokens from `owner`.
+    #[ink(event)]
+    #[derive(Debug)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        value: Balance,
+    }
 
     #[ink(storage)]
     #[derive(Default, Storage)]
@@ -23,11 +51,46 @@ pub mod my_psp22 {
         psp22: psp22::Data,
         #[storage_field]
         metadata: metadata::Data,
+        holders: Balance,
+        // If holder held tokens before, but transfered all of his tokens (balance = 0), assign false.
+        is_holder :Mapping<AccountId, bool>
     }
+
+    pub type Event = <MyPsp22 as ContractEventBase>::Type;
 
     impl PSP22 for MyPsp22 {}
 
     impl PSP22Metadata for MyPsp22 {}
+
+    impl psp22::Internal for MyPsp22 {
+        fn _emit_transfer_event(
+            &self,
+            _from: Option<AccountId>,
+            _to: Option<AccountId>,
+            _amount: Balance,
+        ) {
+            MyPsp22::emit_event(
+                self.env(),
+                Event::Transfer(Transfer {
+                    from: _from,
+                    to: _to,
+                    value: _amount,
+                }),
+            );
+
+        }
+
+        fn _emit_approval_event(&self, _owner: AccountId, _spender: AccountId, _amount: Balance) {
+            MyPsp22::emit_event(
+                self.env(),
+                Event::Approval(Approval {
+                    owner: _owner,
+                    spender: _spender,
+                    value: _amount,
+                }),
+            )
+        }
+    }
 
     impl MyPsp22 {
         #[ink(constructor)]
@@ -37,6 +100,8 @@ pub mod my_psp22 {
             instance.metadata.name = name;
             instance.metadata.symbol = symbol;
             instance.metadata.decimals = decimal;
+            let is_holder_mapping = Mapping::default();
+            instance.is_holder = is_holder_mapping;
             instance
                 ._mint_to(instance.env().caller(), total_supply)
                 .expect("Should mint total_supply");
@@ -44,5 +109,12 @@ pub mod my_psp22 {
             instance
 
         }
+
+        // Emit event abstraction. Otherwise ink! deserializes events incorrectly when there are events from more than one contract.
+        pub fn emit_event<EE: EmitEvent<Self>>(emitter: EE, event: Event) {
+            emitter.emit_event(event);
+        }
+
+
     }
 }
